7fe91681206c5d5d0f3e55d5d4302e5473881573
RyanMcd29 <117286428+RyanMcd29@users.noreply.github.com>
2022-12-26 16:43:38 +0800

    Added ingredient list object

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+// Ingredient Lists
+var ingredientList =
+    [
+        category = ['Cocktail', 'Shot', 'Shake', 'Any'],
+
+        spirits = ['Vodka', 'Gin', 'Whiskey', 'Absinthe', 'Rum',
+                    'Wine', 'Beer', 'Liqueur', 'Champagne', 'Tequila',
+                    'Any'  ],
+
+        mixers = [  'Orange Juice', 'Milk', 'Lemon Juice', 'Orange Bitters', 
+                    'Coffee', 'Any'   ],
+
+        glassType = [   'Coupe Glass', 'Martini Glass', 
+                        'Highball Glass', 'Margarita Glass', 'Shot Glass',
+                        'Any'   ],
+
+        firstLetter = [ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 
+                        'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 
+                        'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',    ],
+    ]
